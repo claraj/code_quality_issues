d84a25365440c785d3fd7ab2c10ae7f1bd1261a9
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-04-21 13:09:45 -0500

    Move while loop up

--- convert_state.py
+++ convert_state.py
@@ -15,8 +15,6 @@ def main():
     for statekey, statevalue in stateAbbr.items() :
         stateAbbr2[statevalue] =  statekey
 
-
-
     #loop
     while True:
         print('1. Convert state to abbreviation')
