42d9ef3108e659b8d4cc3bb00b0867a689454631
John Cokins <il7148pg@go.minnstate.edu>
2021-04-21 14:25:19 -0500

    typos, unnecessary comments removed

--- convert_state.py
+++ convert_state.py
@@ -1,25 +1,26 @@
-"""This code converts written state names to the 2 letter abbreviations, or the reverse"""
+"""
+This code converts written state names to the 2 letter abbreviations, or the reverse
+"""
 import json
 
 def main():
     """
-    reads in the data from state_data json file
+    Reads in the data from state_data json file
     """
     with open('state_data.json','r') as file:
         state_data = file.read()
         state_abbr_json = json.loads(state_data)
 
     # Create a dictionary of states -> abbreviations, and a dictionary of abbreviations -> states 
-    # dictionary of state abbreviations keys and state name values
     state_abbr_to_longform = state_abbr_json  
-    state_longform_to_abbr = {} # dictionary of state name keys and state abbreviation values
+    state_longform_to_abbr = {} 
     # state key is the key in the state dictionary. state value is the value in the dictionary 
-    for statekey, statevalue in state_abbr_to_longform.items() :
-        state_longform_to_abbr[statevalue] = statekey
+    for state_key, state_value in state_abbr_to_longform.items() :
+        state_longform_to_abbr[state_value] = state_key
 
     while True:
         print('1. Convert state to abbreviation ')
-        print('2. Convery abreviasion to state ')
+        print('2. Convert abbreviation to state ')
         print('3. Quit ')
         choice = input('Enter choice: ')
 
@@ -37,7 +38,7 @@ def main():
             
 def convert_state_to_abbreviation(dictionary):
     """
-    function converts full written state to abbreviation, e.g. 'Minnesota' to 'MN'
+    Function converts full written state to abbreviation, e.g. 'Minnesota' to 'MN'
     """
     user_input = input('Enter state name ').capitalize()
     result = dictionary.get(user_input)
