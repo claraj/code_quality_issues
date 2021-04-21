110189652ee8e9016cae984598b7e2a0305d60b0
Nelwell <nick.elwell206@gmail.com>
2021-04-21 13:37:06 -0500

    fix line too long and additional formatting

--- convert_state.py
+++ convert_state.py
@@ -8,8 +8,8 @@ def main():
     print(state_names)
 
     # create a dictionary of states -> abbreviations, and a dictionary of abbreviations -> states
-    state_abbr_to_names_dict = state_names  # dictionary of state abbreviations keys and state name values
-    state_names_to_abbr = {}  # dictionary of state name keys and state abbreviation values
+    state_abbr_to_names_dict = state_names
+    state_names_to_abbr = {}
 
     # state key is the key in the state dictionary. State value is the value in the dictionary
     for state_key, state_value in state_abbr_to_names_dict.items():
@@ -41,7 +41,7 @@ def convert_state_to_abbreviation(state_names_to_abbr):
 
 
 def convert_abbreviation_to_state(state_abbr_to_names_dict):
-    user_input = input('Enter abbreviation name').upper()
+    user_input = input('Enter abbreviation name: ').upper()
     result = state_abbr_to_names_dict.get(user_input)
     if result == None:
         print('Abbreviation not found.')
