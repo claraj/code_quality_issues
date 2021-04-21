4a1cfa4665fc0266e6db34c6d2dc637cd9c9daed
Orion <gn9081ca@go.minneapolis.edu>
2021-04-21 13:21:36 -0500

    Changed some grammar issues in existing comments

--- convert_state.py
+++ convert_state.py
@@ -10,15 +10,15 @@ def main():
     print(data)
     
     # Create a dictionary of states -> abbreviations, and a dictionary of abbreviations -> states 
-    stateAbbr = data #dictionary of state abbreviations keys and state name values 
-    stateAbbr2 = {}# dictionary of state name keys and state abbreviation values
+    stateAbbr = data    # dictionary of state abbreviation keys and state name values 
+    stateAbbr2 = {} # dictionary of state name keys and state abbreviation values
     # state key is the key in the state dictionary. state value is the value in the dictionary 
     for statekey, statevalue in stateAbbr.items() :
         stateAbbr2[statevalue] =  statekey
 
 
 
-    #loop
+    # loop
     while True:
         print('1. Convert state to abbreviation')
         print('2. Convery abreviasion to state')
