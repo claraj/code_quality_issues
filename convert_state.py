be4a2c09a166a8203c0ce2dc43c5502938b50c10
Aaron Goranson <aj.goranson5@gmail.com>
2021-04-21 14:35:56 -0400

    Touches on code quality

--- convert_state.py
+++ convert_state.py
@@ -1,22 +1,17 @@
 import json  # import json for processing
-import pprint
+
 
 
 def main():
     with open('data.json','r') as file:
         jsondata = file.read()
         loaded_data = json.loads(jsondata)
-
-    
+ 
     # Create a dictionary of states -> abbreviations, and a dictionary of abbreviations -> states 
     state_name_keys = loaded_data # dictionary of state abbreviations keys and state name values 
     state_dictionary = {} # dictionary of state name keys and state abbreviation values
-
-
     for state_key, state_value in state_name_keys.items():
         state_dictionary[state_value] =  state_key
-
-
 
     while True:
         print('1: Convert a state to an abbreviation')
@@ -24,7 +19,7 @@ def main():
         print('Press any other number to quit')
         choice = input('Enter choice: ')
 
-        if choice=='1':
+        if choice == '1':
             convertStateToAbbreviation(state_dictionary)
         elif choice == '2':
             convert_abbreviation_to_state(state_name_keys)
@@ -42,13 +37,10 @@ def convertStateToAbbreviation(dictionary):
 def convert_abbreviation_to_state(dictionary):
     user_input = input('Enter abbrviation name: ').upper()
     result = dictionary.get(user_input)
-    if result == None:
+    if result is None:
         print('Abbreviation not found')
     else:
         print(f'The state with the abbreviation {user_input} is {result}')
-
-
-
 
 if __name__ == '__main__':
     main()
