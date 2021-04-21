a730731b11f05af3405239c6b49d72862144f400
Aaron Goranson <aj.goranson5@gmail.com>
2021-04-21 15:10:03 -0400

    Fix code

--- convert_state.py
+++ convert_state.py
@@ -1,14 +1,14 @@
-"""This python program converts states in abbreviations and abbreviations into states """
-
+"""
+This python program converts states in abbreviations and abbreviations into states 
+"""
 import json  # import json for processing
-
-
 
 def main():
     """ Main method """
     with open('data.json','r') as file:
         json_data = file.read()
         json_data = json.loads(json_data)
+    print(json_data)
  
     # Create a dictionary of states -> abbreviations, and a dictionary of abbreviations -> states 
     state_name_keys = json_data # dictionary of state abbreviations keys and state name values 
@@ -19,25 +19,33 @@ def main():
     while True:
         print('1: Convert a state to an abbreviation')
         print('2: Convert an abbreviation to a state')
-        print('Press any other number to quit')
+        print('3: Press 3 to quit')
         choice = input('Enter choice: ')
 
         if choice == '1':
-            convertStateToAbbreviation(state_dictionary)
+            convert_state_to_abbreviation(state_dictionary)
         elif choice == '2':
             convert_abbreviation_to_state(state_name_keys)
-        else:
+        elif choice == '3':
             break
+        else:
+            print('try again')
 
-def convertStateToAbbreviation(dictionary):
-    user_input = input('Enter state name: ').capitalize()
-    result = dictionary.get(user_input)
-    if result == None:
+def convert_state_to_abbreviation(state_dictionary):
+    """
+    Takes states and converts to abbreviation
+    """
+    user_input = input('Enter state name: ').title()
+    result = state_dictionary.get(user_input)
+    if result is None:
         print('State not found')
     else:
         print(f'The abbreviation for {user_input} is {result}')
 
 def convert_abbreviation_to_state(dictionary):
+    """
+    Take state abbreviation to a state
+    """
     user_input = input('Enter abbrviation name: ').upper()
     result = dictionary.get(user_input)
     if result is None:
