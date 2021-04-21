a229c52db1d3f3de274abb6994ffc7dd45fa98e9
PickleEric <60155510+PickleEric@users.noreply.github.com>
2021-04-21 14:01:10 -0500

    fixed spacing updated spelling

--- convert_state.py
+++ convert_state.py
@@ -1,23 +1,21 @@
 import json  # import json for json processing
+
 def main():
     with open('data.json','r') as file:
         data = file.read()
         stateData = json.loads(data)
 
-    print(stateData)
-    
-    # Create a dictionary of states -> abbreviations, and a dictionary of abbreviations -> states 
     stateKeyandName = stateData 
     stateKeyandAbbrev = {}
 
     for statekey, statevalue in stateKeyandName.items() :
-        stateKeyandAbbrev[statevalue] =  statekey
+        stateKeyandAbbrev[statevalue] = statekey
 
     while True:
         print('1. Convert state to abbreviation')
         print("2. Convert abbreviation to state")
         print('3. Quit')
-        choice =   input("Enter choice : ")
+        choice = input("Enter choice:")
 
         if choice=="1":
             convert_state_to_abbreviation(stateKeyandAbbrev)
@@ -29,8 +27,8 @@ def main():
             print('Try Again')
 
 def convert_state_to_abbreviation(dictionary ):
-    userInput = input("Enter state name").capitalize()
-    result = dictionary.get(userInput  )
+    userInput = input("Enter state name:").capitalize()
+    result = dictionary.get(userInput)
 
     if result == None:
         print('State Not found')
@@ -39,16 +37,12 @@ def convert_state_to_abbreviation(dictionary ):
 
 def convert_abbreviation_to_state(dictionary):
 
-    userInput = input("Enter abbrviation name.").upper()
+    userInput = input("Enter abbrviation name:").upper()
     result = dictionary.get(userInput)
     if result == None:
         print('Abreviation not found.')
     else:
         print("The state with the abbreviation." + userInput + " is " + result)
-
-
-
-
 
 if __name__ == "__main__":
 
