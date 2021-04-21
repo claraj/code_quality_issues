7cf1d41d57d4688c3ef0c000be062b260675a868
John Cokins <il7148pg@go.minnstate.edu>
2021-04-21 13:59:47 -0500

    added docstrings, changed double quote to single quote

--- convert_state.py
+++ convert_state.py
@@ -1,6 +1,10 @@
+"""This code converts written state names to the 2 letter abbreviations, or the reverse"""
 import json
 
 def main():
+    """
+    reads in the data from state_data json file
+    """
     with open('state_data.json','r') as file:
         state_data = file.read()
         state_abbr_json = json.loads(state_data)
@@ -17,10 +21,10 @@ def main():
         print('1. Convert state to abbreviation ')
         print('2. Convery abreviasion to state ')
         print('3. Quit ')
-        choice =   input('Enter choice: ')
+        choice = input('Enter choice: ')
 
         if choice == '1':
-            convertStateToAbbreviation(state_longform_to_abbr)
+            convert_state_to_abbreviation(state_longform_to_abbr)
 
         elif  choice == '2':
             convert_abbreviation_to_state(state_abbr_to_longform)
@@ -31,23 +35,29 @@ def main():
         else:
             print('try again ')
             
-def convertStateToAbbreviation(dictionary):
+def convert_state_to_abbreviation(dictionary):
+    """
+    function converts full written state to abbreviation, e.g. 'Minnesota' to 'MN'
+    """
     user_input = input('Enter state name ').capitalize()
     result = dictionary.get(user_input)
 
-    if result == None:
+    if result is None:
         print('state not found ')
     else:
         print ('The abbreviation for ' +  user_input + ' is ' + result)
 
 def convert_abbreviation_to_state(dictionary):
+    """
+    Converts state abbreviation to full name, e.g. 'WI' to 'Wisconsin'
+    """
     user_input = input('Enter abbrviation name ').upper()
     result = dictionary.get(user_input)
-    
+
     if result is None:
         print('abreviation not found')
     else:
-        print("the state with the abbreviation " + user_input + " is " + result)
+        print('the state with the abbreviation ' + user_input + ' is ' + result)
 
 
 if __name__ == "__main__":
