726463e1b97e24da76baa972f25b00fc59dd69fb
Mohammad <Email>
2021-04-25 12:39:54 -0500

    update 3

--- convert_state.py
+++ convert_state.py
@@ -1,14 +1,20 @@
-
-
-
+"""
+Conver between state names and abbreviation
+"""
 import json  # import json for json processing
+
 def main():
+
+""" Main function, runs menu """
+
     with open('data.json','r') as file:
         state_names = file.read()
         state_names_data = json.loads(data)
+
     # Create a dictionary of states -> abbreviations, and a dictionary of abbreviations -> states 
     state_abbr = state_names_data # dictionary of state abbreviations keys and state name values 
     state_abbr2 = {} # dictionary of state name keys and state abbreviation values
+
     # state key is the key in the state dictionary. state value is the value in the dictionary 
     for state_key, state_value in state_abbr.items() :
         state_abbr2[state_value] = state_key
@@ -19,6 +25,7 @@ def main():
         print('2. Convert abreviasion to state')
         print('3. quit')
         choice = input('Enter choice : ')
+
         if choice == '1':
             convert_state_to_abbreviation(state_abbr2)
         elif choice == '2':
@@ -29,15 +36,24 @@ def main():
             print('try again')
 
 def convert_state_to_abbreviation(dictionary ):
+
+    """ Converts state name to abbrevation """
+
     user_input = input('Enter state name').  capitalize()
     result = dictionary.get(user_input)
+
     if result is None:
         print('state not found')
     else:
         print ('The abbreviation for ' +  user_input+ ' is ' + result)
+
 def convert_abbreviation_to_state(dictionary):
+
+    """ Converts Abbr to state name """
+
     user_input   = input('Enter abbrviation name').upper()
     result = dictionary.get(user_input)
+    
     if result is None:
         print('abreviation not found')
     else:
