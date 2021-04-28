6dff328e60a001cb583375b58c5536aa3d31f7d1
PickleEric <60155510+PickleEric@users.noreply.github.com>
2021-04-28 14:16:06 -0500

    Capitalization made consistant

--- convert_state.py
+++ convert_state.py
@@ -6,7 +6,7 @@ def main():
 
     """ Main function creates dictionary of states and runs menu """
 
-    with open('data.json','r') as file:
+    with open('data.json', 'r') as file:
         data = file.read()
         state_data = json.loads(data)
 
@@ -18,15 +18,15 @@ def main():
 
     while True:
         print('1. Convert state to abbreviation')
-        print("2. Convert abbreviation to state")
+        print('2. Convert abbreviation to state')
         print('3. Quit')
-        choice = input("Enter choice:")
+        choice = input('Enter choice:')
 
-        if choice=="1":
+        if choice == '1':
             convert_state_to_abbreviation(state_key_and_abbrev)
-        elif  choice=="2":
+        elif  choice == '2':
             convert_abbreviation_to_state(state_key_and_name)
-        elif choice=="3":
+        elif choice == '3':
             break
         else:
             print('Try Again')
@@ -35,24 +35,24 @@ def convert_state_to_abbreviation(dictionary ):
 
     """ Take state name and convert to abbreviation"""
 
-    user_input = input("Enter state name:").capitalize()
+    user_input = input('Enter state name:').capitalize()
     result = dictionary.get(user_input)
 
     if result is None:
         print('State Not found')
     else:
-        print ( "The abbreviation for " + user_input + " is " + result + ".")
+        print ( 'The abbreviation for ' + user_input + ' is ' + result + '.')
 
 def convert_abbreviation_to_state(dictionary):
 
     """ Take abbreviation to state name"""
 
-    user_input = input("Enter abbrviation name:").upper()
+    user_input = input('Enter abbrviation name:').upper()
     result = dictionary.get(user_input)
     if result is None:
-        print('Abreviation not found.')
+        print('Abreviation Not Found.')
     else:
-        print("The state with the abbreviation." + user_input + " is " + result)
+        print('The state with the abbreviation.' + user_input + ' is ' + result)
 
 if __name__ == "__main__":
     main()
