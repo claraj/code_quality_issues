41affc6f920eb65822af848637985d4987fc395c
Jake-Fischer <jakefischer000@gmail.com>
2021-12-01 21:14:58 -0600

    Change all quotes to single quotes

--- convert_state.py
+++ convert_state.py
@@ -16,37 +16,37 @@ def main():
         stateAbbr2[statevalue] =  statekey
     while True:
         print('1. Convert state to abbreviation')
-        print("2. Convery abreviasion to state")
+        print('2. Convery abreviasion to state')
         print(' 3. quit')
-        choice =   input("Enter choice : ")
+        choice =   input('Enter choice : ')
 
-        if choice=="1":
+        if choice=='1':
             convertStateToAbbreviation(stateAbbr2)
-        elif  choice == "2":
+        elif  choice == '2':
             convert_abbreviation_to_state(stateAbbr)
-        elif choice==  "3":
+        elif choice==  '3':
             break
         else:
             print('try again')
 
 
 def convertStateToAbbreviation(dictionary ):
-    userInput   = input("Enter state name").  capitalize()
+    userInput   = input('Enter state name').  capitalize()
     result = dictionary.get(userInput  )
     if result == None:
         print(' state not found')
     else:
-        print ( "The abbreviation for " +  userInput+" is " + result)
+        print ( 'The abbreviation for ' +  userInput+' is ' + result)
 
 
 def convert_abbreviation_to_state(dictionary):
-    userInput   = input("Enter abbrviation name").upper()
+    userInput   = input('Enter abbrviation name').upper()
     result = dictionary.get(userInput)
     if result == None:
         print('error')
     else:
-        print("the state with the abbreviation   " + userInput + " is " + result)
+        print('the state with the abbreviation   ' + userInput + ' is ' + result)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
