a834bb4c92d90ed8acea196c4d8e4b1c9d9b0cbf
Jake-Fischer <jakefischer000@gmail.com>
2021-12-01 21:36:16 -0600

    Few minor changes to a variable name, removed a space

--- convert_state.py
+++ convert_state.py
@@ -3,19 +3,19 @@ import json
 
 def main():
     with open('code_quality_issues/data.json','r') as file:
-        data = file.read()
-        json_data = json.loads(data)
-    state_abbreviation = json_data # Create a dictionary of abbreviations -> states
+        state_data = file.read()
+        json_state_data = json.loads(state_data)
+    state_abbreviation = json_state_data # Create a dictionary of abbreviations -> states
     state_state_abbreviation_2 = {} # Initiate a new dictionary to hold states -> abbreviations
+
     # Populate stateAbbr2 with states -> abbreviations
-    for state_key, state_value in state_abbreviation.items() :
+    for state_key, state_value in state_abbreviation.items():
         state_state_abbreviation_2[state_value] =  state_key
     while True:
         print('1. Convert state to abbreviation')
-        print('2. Convery abreviasion to state')
+        print('2. Convery abbreviation to state')
         print('3. Quit')
         choice = input('Enter choice: ')
-
         if choice == '1':
             convert_state_to_abbreviation(state_state_abbreviation_2)
         elif choice == '2':
@@ -36,7 +36,7 @@ def convert_state_to_abbreviation(dictionary):
 
 
 def convert_abbreviation_to_state(dictionary):
-    user_input = input('Enter abbrviation name: ').upper()
+    user_input = input('Enter abbreviation name: ').upper()
     result = dictionary.get(user_input)
     if result == None:
         print('Error')
